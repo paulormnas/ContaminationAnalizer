77ec0df61b9e608ec4d7a6bae0f0b22e8c692239
Paulo Nascimento <paulormnas@gmail.com>
2018-02-21 10:28:19 -0300

    Implemented verification of point inside polygon, but need to test

--- test_graph.py
+++ test_graph.py
@@ -15,6 +15,8 @@ class CEnvironmentGraph():
         self.g = Graph()  # Create new graph object
         self.species = species
         self.connections = connections
+        self.w_width = 0
+        self.w_height = 0
         self.max_vertex = 5000
         self.v_total = 0
         self.pixel_step = 20
@@ -56,8 +58,8 @@ class CEnvironmentGraph():
         scale_xy = min(width_ratio, height_ratio)
 
         v_count = 0
-        for i in range(len(self.shapes)):
-        # for i in range(0, 10):
+        # for i in range(len(self.shapes)):
+        for i in range(0, 1):
             if v_count < self.max_vertex:
                 coords = self.shapes[i].points
                 x_max, x_min, y_max, y_min = coords[0][0], coords[0][0], coords[0][1], coords[0][1]
@@ -90,7 +92,12 @@ class CEnvironmentGraph():
                     if v_count < self.max_vertex:
                         for row in range(0, int(self.w_height / 3), self.pixel_step):
                             # print(x_min, column, x_max, y_min, row, y_max)
-
+                            # if self.is_inside(coords, len(coords) - 1, list(column, row)):
+                            #     if v_count < self.max_vertex and [column, row] not in self.v_pos:
+                            #         self.v_pos.append([column, row])
+                            #         v_count += 1
+                            #     else:
+                            #         break
                             if x_min <= column <= x_max and y_min <= row <= y_max:
                                 if v_count < self.max_vertex and [column, row] not in self.v_pos:
                                     self.v_pos.append([column, row])
@@ -103,6 +110,146 @@ class CEnvironmentGraph():
                 break
         print("Vertices total:", v_count)
         self.v_total = v_count - 1
+
+    @staticmethod
+    def on_segment(p, r, q):
+        """
+        Given three collinear points p, q, r, the function checks if point q lies on line segment 'pr'
+        :param p: List with x and y coordinates of first point of the segment 'pr'
+        :type p: list
+        :param r: List with x and y coordinates of second point of the segment 'pr'
+        :type r: list
+        :param q: List with x and y coordinates of point to verify collinearity
+        :type q: list
+        :return: Return True if the point q is collinear to segment 'pr', False otherwise.
+        :rtype: bool
+        """
+        if min(p[0], r[0]) <= q[0] <= max(p[0], r[0]) and \
+                min(p[1], r[1]) <= q[1] <= max(p[1], r[1]):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def orientation(p, q, r):
+        """
+        To find orientation of ordered triplet (p, q, r).
+        The function returns following values
+        0 --> p, q and r are collinear
+        1 --> Clockwise
+        2 --> Counterclockwise
+        :param p: List with x and y coordinates of first point of the segment 'pq'
+        :type p: list
+        :param q: List with x and y coordinates of second point of the segment 'pq'
+        :type q: list
+        :param r: List with x and y coordinates of point to verify the orientation
+        :type r: list
+        :return: Return a integer correspondent to the orientation of segment 'pq' related to point r
+        :rtype: int
+        """
+        # See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+        # for details of below formula.
+        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+        if val == 0:
+            return 0  # collinear
+
+        elif val > 0:
+            return 1  # clockwise
+        else:
+            return 2  # counterclockwise
+
+    def do_intersect(self, p1, q1, p2, q2):
+        """
+        This function verify if line segment 'p1q1' and 'p2q2' intersect each other.
+        :param p1: List with x and y coordinates of first point of the segment 'p1q1'
+        :type p1: list
+        :param q1: List with x and y coordinates of second point of the segment 'p1q1'
+        :type q1: list
+        :param p2: List with x and y coordinates of first point of the segment 'p2q2'
+        :type p2: list
+        :param q2: List with x and y coordinates of second point of the segment 'p2q2'
+        :type q2: list
+        :return: Return True if segment 'p1q1' intersects segment 'p2q2'
+        :rtype: bool
+        """
+        # Find the four orientations needed for general and special cases
+        o1 = self.orientation(p1, q1, p2)
+        o2 = self.orientation(p1, q1, q2)
+        o3 = self.orientation(p2, q2, p1)
+        o4 = self.orientation(p2, q2, q1)
+
+        # General case
+        if o1 != o2 and o3 != o4:
+            return True
+
+        # Special Cases
+        # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+        if o1 == 0 and self.on_segment(p1, q1, p2):
+            return True
+
+        # p1, q1 and p2 are collinear and q2 lies on segment p1q1
+        if o2 == 0 and self.on_segment(p1, q1, q2):
+            return True
+
+        # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+        if o3 == 0 and self.on_segment(p2, q2, p1):
+            return True
+
+        # p2, q2 and q1 are collinear and q1 lies on segment p2q2
+        if o4 == 0 and self.on_segment(p2, q2, q1):
+            return True
+
+        return False  # Doesn't fall in any of the above cases
+
+    def is_inside(self, polygon, n, p):
+        """
+        Returns true if the point p lies inside the polygon[] with n vertices
+        :param polygon: List of x and y coordinates that compose the polygon
+        :type polygon: list
+        :param n: Polygon numberr of vertices
+        :type n: int
+        :param p: Point to verify if is inside of polygon
+        :type p: list
+        :return: Return True if the number of intersections is odd, which means the point is inside the polygon,
+        False otherwise
+        :rtype: bool
+        """
+        # There must be at least 3 vertices in polygon[]
+        if n < 3:
+            return False
+
+        # Create a point for line segment from p to infinite
+        extreme = [self.w_width, p[1]]
+
+        # Count intersections of the above line with sides of polygon
+        count = 0
+        i = 0
+        while True:
+            next = (i+1) % n
+            p1 = polygon[i]
+            q1 = polygon[next]
+
+            # Normalize maximum and minimum values of shape coordinates and convert to widget dimensions
+            p1[0] = (p1[0] - self.x1) / (self.x2 - self.x1)
+            p1[1] = (p1[1] - self.y1) / (self.y2 - self.y1)
+            q1[0] = (q1[0] - self.x1) / (self.x2 - self.x1)
+            q1[1] = (q1[1] - self.y1) / (self.y2 - self.y1)
+
+            # Check if the line segment from 'p' to 'extreme' intersects
+            # with the line segment from 'polygon[i]' to 'polygon[next]'
+            if self.do_intersect(polygon[i], polygon[next], p, extreme):
+                # If the point 'p' is collinear with line segment 'i-next',
+                # then check if it lies on segment. If it lies, return true,
+                # otherwise false
+                if self.orientation(polygon[i], p, polygon[next]) == 0:
+                    return self.on_segment(polygon[i], polygon[next], p)
+                count += 1
+            i = next
+            if i == 0:
+                break
+
+        # Return true if count is odd, false otherwise
+        return count & 1  # Same as (count%2 == 1)
 
     def add_vertices(self):
         """
